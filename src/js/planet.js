c85c079fe2106959c07fe32d4c4e009a9c09b42f
Vanessa Su <vnessa.su@gmail.com>
2021-04-16 13:07:05 -0400

    Add earthYearsPerYear attribute

--- src/js/planet.js
+++ src/js/planet.js
@@ -2,5 +2,6 @@ export default class Planet{
   constructor(planetName, earthDaysPerYear){
     this.name = planetName;
     this.earthDaysPerYear = earthDaysPerYear;
+    this.earthYearsPerYear = Number((365 / earthDaysPerYear).toFixed(2));
   }
 }
